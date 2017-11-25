3351da400b72ad40ed59e216dedd420a9ff9ea5c
Andrew Nicholson <andrew.nicholson4491@gmail.com>
2017-11-25 23:38:58 +0000

    refactored chart to use recharts instead of victory

--- client/temperature-display.js
+++ client/temperature-display.js
@@ -1,41 +1,43 @@
 import React from 'react';
 import { Mongo } from 'meteor/mongo';
 import { withTracker } from 'meteor/react-meteor-data'
-import { VictoryChart, VictoryLine, VictoryTheme } from 'victory';
 import tuc from 'temp-units-conv';
-import { LineChart, XAxis, Tooltip, CartesianGrid, Line } from 'recharts';
+import { LineChart, XAxis, YAxis, Tooltip, CartesianGrid, Line } from 'recharts';
+import moment from 'moment';
 
 import { TemperatureLogs } from '../imports/api/temperature-logs';
 
 export class TemperatureDisplay extends React.PureComponent {
   currentTemp() {
-    const currentTempLog = this.props.temperatureLogs[0];
+    const currentTempLog = this.props.temperatureLogs[this.props.temperatureLogs.length-1];
     return currentTempLog ? tuc.c2f(currentTempLog.temperature) : null;
   }
 
   render() {
-    const data = this.props.temperatureLogs.map((log) => ({x: log.date, y: tuc.c2f(log.temperature)}));
+    const { temperatureLogs } = this.props;
+    const data = temperatureLogs.map((log) =>
+       ({
+        date: moment(log.date).format('M/D/YY HH:mm'),
+        temperature: tuc.c2f(log.temperature)
+        }));
     return (
       <div>
-       {this.currentTemp()}
-	<VictoryChart
-	  theme={VictoryTheme.material}
-	  scale={{x: 'time'}}
-	>
-	  <VictoryLine
-	    style={{
-	      data: { stroke: "#c43a31" },
-	      parent: { border: "1px solid #ccc"}
-	    }}
-	    data={data}
-	  />
-	</VictoryChart>
+        <dl>
+            <dt>Current Temperature</dt>
+            <dd>{this.currentTemp()} F</dd>
+        </dl>
+        <LineChart width={1750} height={750} data={data}>
+          <Tooltip />
+          <XAxis dataKey="date" interval={Math.floor(temperatureLogs.length / 10)} />
+          <YAxis domain={[]} unit="F" />
+          <CartesianGrid stroke="#eee" strokeDasharray="5 5"/>
+          <Line type="monotone" unit="F" dataKey="temperature" stroke="#8884d8" />
+        </LineChart>
       </div>);
   }
 }
 
 export const TemperatureDisplayContainer = withTracker(() => {
-  //const handler = Meteor.subscribe('temperatureLog');
-  const temperatureLogs = TemperatureLogs.find({}, {sort: { date: -1}}).fetch();
+  const temperatureLogs = TemperatureLogs.find({}, {sort: { date: 1}}).fetch();
   return { temperatureLogs }
 })(TemperatureDisplay);
