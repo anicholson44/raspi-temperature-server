0b17386b077382fe8c2887fd39e390a5f7afa568
Andrew Nicholson <andrew.nicholson4491@gmail.com>
2017-11-30 01:31:17 +0000

    setup script
    installs systemctl service to start meteor service on startup and
    crontab to record temp every 5 minutes

---
+++ bin/setup.sh
@@ -0,0 +1,12 @@
+SERVICE_DIR=/lib/systemd/system/
+SCRIPT_PATH="$( cd "$(dirname "$0")" ; pwd -P )"
+
+sudo cp $SCRIPT_PATH/raspi-temperature-server.service $SERVICE_DIR
+
+sudo systemctl start raspi-temperature-server.service
+sudo systemctl enable raspi-temperature-server.service
+
+cp $SCRIPT_PATH/record-temp ~/bin
+chmod +x ~/bin/record-temp
+
+crontab -u pi crontab
