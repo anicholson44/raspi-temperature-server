2a9e989000422f8178e7054fe3cc012526126c1b
Andrew Nicholson <andrew.nicholson4491@gmail.com>
2017-11-30 01:46:36 +0000

    fixed setup script

--- bin/setup.sh
+++ bin/setup.sh
@@ -6,7 +6,8 @@ sudo cp $SCRIPT_PATH/raspi-temperature-server.service $SERVICE_DIR
 sudo systemctl start raspi-temperature-server.service
 sudo systemctl enable raspi-temperature-server.service
 
+mkdir ~/bin
 cp $SCRIPT_PATH/record-temp ~/bin
 chmod +x ~/bin/record-temp
 
-crontab -u pi crontab
+crontab -u pi $SCRIPT_PATH/crontab
