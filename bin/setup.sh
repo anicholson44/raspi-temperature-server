82d4b37d63dde733c5b9e05e3853a175e935ce7e
Andrew Nicholson <andrew.nicholson4491@gmail.com>
2017-12-25 15:19:04 -0500

    added temperature sensor config to setup script

--- bin/setup.sh
+++ bin/setup.sh
@@ -1,6 +1,10 @@
 SERVICE_DIR=/lib/systemd/system/
 SCRIPT_PATH="$( cd "$(dirname "$0")" ; pwd -P )"
 
+cat 'dtoverlay=w1-gpio' | sudo tee -a /boot/config.txt > /dev/null
+sudo modprobe w1-gpio
+sudo modprobe w1-therm
+
 sudo cp $SCRIPT_PATH/raspi-temperature-server.service $SERVICE_DIR
 
 sudo systemctl start raspi-temperature-server.service
